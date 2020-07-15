5bb0e49cec7a2cc51118013e47ea7e9fb3f9c1de
Shane McDonough <shanemcdman@gmail.com>
2020-07-14 17:45:44 -0700

    Add testing lines

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main(){
-	cout << "Hello World!" << endl;
+    //constructors
+    Vec2 v1;
+    Vec2 v2(5, 5);
+    cout << v1.get_x() << " " << v1.get_y() << endl;
+    cout << v2.get_x() << " " << v2.get_y() << endl;
+    v1.set_x(3);
+    v1.set_y(3);
+    cout << v1.get_x() << " " << v1.get_y() << endl;
 	return 0;
 }
